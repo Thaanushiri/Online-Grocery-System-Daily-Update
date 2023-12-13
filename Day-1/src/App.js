7d09a4a2e54ff20c924cec5b2d6db406789446de
thaanushri2005 <22csecs.thaanushri.g@skct.edu.in>
2023-12-13 12:27:52 +0530

    updated day1

---
+++ Day-1/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import './App.css';
+const Register = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [address, setAddress] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = () => {
+    // Add your registration logic here
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Address:', address);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+    // You can add user registration logic or redirect to another page as needed
+  };
+
+  return (
+    <div className="register-container">
+      <form className="register-form">
+        <h2>Register</h2>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="address">Address:</label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleRegister}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
